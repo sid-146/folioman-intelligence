dd6274f68849619d0af96e187e051a607dc8d910
Sudhawna-pc <sudhanwa.kaveeshwar@gmail.com>
2026-09-14 23:52:25 +0530

    updated examples

---
+++ holding_example.py
@@ -0,0 +1,11 @@
+import asyncio
+from src.folioman_intelligence.analytics.portfolio import holding_details
+
+
+async def main(isin: str):
+    response = await holding_details(1, 8)
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main("INF966L01689"))
--- llm_analytics_example.py
+++ llm_analytics_example.py
@@ -2,9 +2,18 @@ from src.folioman_intelligence.agents.portfolio import ask_portfolio_agent
 import asyncio
 import pprint
 
+# response = asyncio.run(
+#     ask_portfolio_agent(
+#         """Analyze my portfolio and provide summary. With Suggestions for improvement.
+#         \ntell me extra details about quant Small Cap Fund - Direct Plan - Growth (Non Demat)"""
+#     )
+# )
+
+# response = asyncio.run(ask_portfolio_agent("""How is my portfolio performing?"""))
+# pprint.pprint(response)
+
+
 response = asyncio.run(
-    ask_portfolio_agent(
-        "Analyze my portfolio and provide summary. With Suggestions for improvement."
-    )
+    ask_portfolio_agent("""What are my biggest portfolio concentrations?""")
 )
 pprint.pprint(response)
